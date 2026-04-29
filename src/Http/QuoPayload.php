cf493ada06fb23e807e9ae187a7efb1a773e4ab1
Quinten Schorsij <quinten@protoqol.nl>
2026-04-30 00:45:18 +0200

    (fix) phpcs errors

--- src/Http/QuoPayload.php
+++ src/Http/QuoPayload.php
@@ -210,9 +210,9 @@ class QuoPayload
 
         if ($type === 'array') {
             $type .= '<' . implode(', ', array_map(function ($item) {
-                    if (function_exists('get_debug_type')) {
-                        return get_debug_type($item);
-                    }
+                if (function_exists('get_debug_type')) {
+                    return get_debug_type($item);
+                }
 
                     $itemType = is_object($item) ? get_class($item) : gettype($item);
 
@@ -223,7 +223,7 @@ class QuoPayload
                     ];
 
                     return $map[$itemType] ?? $itemType;
-                }, $this->variable)) . '>';
+            }, $this->variable)) . '>';
         }
 
         return $type;
